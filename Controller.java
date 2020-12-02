9ac4fa837f8ef2bd591233595d6c0f869665e205
drunkpolishbear <donaldtgina@gmail.com>
2020-12-02 12:02:01 -0700

    I don't fucking know, I probably made it worse

--- Controller.java
+++ Controller.java
@@ -1,19 +1,21 @@
 package xkcd;
 
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
-import javafx.scene.control.Menu;
 import javafx.scene.image.Image;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.VBox;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import org.json.JSONObject;
 
 public class Controller {
     @FXML
@@ -25,16 +27,29 @@ public class Controller {
     public VBox root;
     public MenuBar menuBar;
     public Menu viewMenu, helpMenu;
+    public Label comicTitle;
 
     public int n;
 
+    private void println(Object obj) {
+        System.out.println(obj);
+    }
+
     private void setImg() throws IOException, InterruptedException {
         n  = Integer.parseInt(comicNumber.getText());
+        JSONObject comicInfo = Util.getComicInfo(n);
         Image comic = new Image(Util.getComic(n));
-        comicImage.setFitWidth(comic.getWidth() * 0.75);
-        comicImage.setFitHeight(comic.getHeight() * 0.75);
-        comicImage.setImage(comic);
+        comicTitle.setText(Util.getElement(comicInfo, "title"));
+        comicTitle.setStyle("-fx-font-size: 30");
+        comicTitle.setTranslateX(160);
+        comicTitle.setTranslateY(64);
+        comicImage.setFitWidth(640);
+        comicImage.setFitHeight(640);
+        double x, y;
+        x = comic.getWidth();
+        y = comic.getHeight();
         comicImage.setPreserveRatio(true);
+        comicImage.setImage(comic);
     }
 
     @FXML
